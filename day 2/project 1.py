38ec3d35b0049f4e5cd779950247bc655f1a1297
sagar pal <sagar140524@gmail.com>
2025-08-30 20:20:48 +0530

    changes again

--- day 2/project 1.py
+++ day 2/project 1.py
@@ -3,3 +3,4 @@ import numpy as np
 a=np.genfromtxt("downloads/supermarket_sales_with_nulls.csv",delimiter =",",skip_header=1,filling_values=np.nan)
 # filling value (fill np.nan where missing space), (skip header means the first line name of col ),delimiter tells the rader to split on comma 
 print(np.isnan(a).sum())
+print(a)
